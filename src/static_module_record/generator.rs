4cb92808390df9a61c34b9e51122429a86019904
muji <muji@tmpfs.org>
2021-06-10 09:07:38 +0800

    Setup basic script structure.

--- src/static_module_record/generator.rs
+++ src/static_module_record/generator.rs
@@ -2,6 +2,7 @@
 use anyhow::Result;
 
 use swc_ecma_ast::*;
+use swc_common::DUMMY_SP;
 
 use super::StaticModuleRecord;
 
@@ -17,7 +18,35 @@ impl<'a> Generator<'a> {
     }
 
     /// Create the program script AST node.
-    pub fn create(&self) -> Result<()> {
-        Ok(())
+    pub fn create(&self) -> Result<Script> {
+        let mut script = Script {
+            span: DUMMY_SP,
+            body: Vec::with_capacity(1),
+            shebang: None,
+        };
+
+        let stmt = Stmt::Expr(ExprStmt {
+            span: DUMMY_SP,
+            expr: Box::new(Expr::Paren(ParenExpr {
+                span: DUMMY_SP,
+                expr: Box::new(Expr::Arrow(ArrowExpr {
+                    span: DUMMY_SP,
+                    params: Vec::new(),
+                    body: BlockStmtOrExpr::BlockStmt(BlockStmt {
+                        span: DUMMY_SP,
+                        stmts: Vec::new(),
+                    }),
+                    is_async: false,
+                    is_generator: false,
+                    type_params: None,
+                    return_type: None,
+                }))
+            })
+            ),
+        });
+
+        script.body.push(stmt);
+
+        Ok(script)
     }
 }
