24dd07584179e3baddd08892f5f35b209530b65b
muji <muji@tmpfs.org>
2021-06-16 14:45:45 +0800

    Add test specs for export default functions.

--- tests/export-default.rs
+++ tests/export-default.rs
@@ -28,6 +28,30 @@ fn export_default_class() -> Result<()> {
 }
 
 #[test]
+fn export_default_function() -> Result<()> {
+    let expected =
+        std::fs::read_to_string("tests/transform/export-default-function/output.js")?;
+    let result = transform(TransformSource::File(PathBuf::from(
+        "tests/transform/export-default-function/input.js",
+    )))?;
+    //print!("{}", &result.code);
+    assert_eq!(expected, result.code);
+    Ok(())
+}
+
+#[test]
+fn export_default_arrow_function() -> Result<()> {
+    let expected =
+        std::fs::read_to_string("tests/transform/export-default-arrow-function/output.js")?;
+    let result = transform(TransformSource::File(PathBuf::from(
+        "tests/transform/export-default-arrow-function/input.js",
+    )))?;
+    //print!("{}", &result.code);
+    assert_eq!(expected, result.code);
+    Ok(())
+}
+
+#[test]
 fn export_default_number() -> Result<()> {
     let expected =
         std::fs::read_to_string("tests/transform/export-default-number/output.js")?;
---
+++ tests/transform/export-default-function/output.js
@@ -0,0 +1,9 @@
+(({ imports: $h‍_imports , liveVar: $h‍_live , onceVar: $h‍_once  })=>{
+    $h‍_imports(new Map([]), []);
+    const { default: $c‍_default  } = {
+        default: function(doSomething) {
+            doSomething();
+        }
+    };
+    $h‍_once.default($c‍_default);
+});
