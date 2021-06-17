bf39e62f74b9873136135e8afb668b62a42a8079
muji <muji@tmpfs.org>
2021-06-17 08:27:29 +0800

    Initial exported name test.

---
+++ tests/export-name.rs
@@ -0,0 +1,16 @@
+use anyhow::Result;
+use std::path::PathBuf;
+
+use basalt::static_module_record::{transform, TransformSource};
+
+#[test]
+fn export_name_1() -> Result<()> {
+    let expected =
+        std::fs::read_to_string("tests/transform/export-name-1/output.js")?;
+    let result = transform(TransformSource::File(PathBuf::from(
+        "tests/transform/export-name-1/input.js",
+    )))?;
+    print!("{}", &result.code);
+    //assert_eq!(expected, result.code);
+    Ok(())
+}
---
+++ tests/transform/export-name-1/input.js
@@ -0,0 +1,5 @@
+export let abc = 123;
+export let def = 456;
+export let def2 = def;
+def++;
+export const ghi = 789;
