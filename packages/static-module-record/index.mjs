a96778cac9db9e62a199e53c0d930f7909ec0faf
muji <muji@tmpfs.org>
2021-06-24 09:47:47 +0800

    Node static module record wrapper program.
    Experimental for testing integration with the compartment-mapper.

---
+++ packages/static-module-record/index.mjs
@@ -0,0 +1,59 @@
+import {spawn} from 'child_process';
+import {readFileSync} from 'fs';
+
+const {freeze, keys} = Object;
+
+const command = "basalt";
+const args = ["transform", "-j", "-"];
+
+/*
+ *  Analyze a module calling out to basalt(1) writing the source
+ *  to stdin and parsing the response JSON payload from stdout.
+ */
+function analyzeModule({source}) {
+
+  return new Promise((resolve, reject) => {
+    let child = spawn(command, args);
+    let buf = Buffer.from([]);
+    child.stdout.on('data', function (chunk) {
+      buf = Buffer.concat([buf, chunk]);
+    });
+    child.stdin.setEncoding('utf-8');
+    child.stdin.write(source);
+    child.stdin.end();
+
+    child.on('error', (e) => reject(e));
+
+    child.on('exit', function(code) {
+      if (code === 0) {
+        return resolve(JSON.parse(buf.toString()));
+      }
+      reject(new Error("Static module record program basalt(1) did not exit successfully"));
+    })
+
+  })
+
+}
+
+export async function parseModule(source, url) {
+  const {
+    meta,
+    program,
+  } = await analyzeModule({ source, url });
+
+  return freeze({
+    imports: freeze([...keys(meta.imports)].sort()),
+    exports: freeze(
+      [...keys(meta.liveExportMap), ...keys(meta.fixedExportMap)].sort(),
+    ),
+    reexports: freeze([...meta.exportAlls].sort()),
+    __syncModuleProgram__: program,
+    __liveExportMap__: meta.liveExportMap,
+    __fixedExportMap__: meta.fixedExportMap,
+  })
+}
+
+// node packages/static-module-record/index.mjs
+//let source = readFileSync("tests/fixtures/static-module-record/main.js");
+//let record = await parseModule(source);
+//console.log(record);
