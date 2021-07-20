d84699765da77456c48b620fbe495df2c9634242
muji <muji@tmpfs.org>
2021-07-20 13:31:01 +0800

    Update for..of test spec for array destructure.

--- tests/globals/scope/for-of-body/input.js
+++ tests/globals/scope/for-of-body/input.js
@@ -1,6 +1,11 @@
 const list = [4,5,6];
+const iterable = new Map([['a', 1], ['b', 2], ['c', 3]]);
 
 for (let i of [1,2,3]) {
   const for_of_foo = document;
   const blah = list[i];
+}
+
+for (const [key, value] of iterable) {
+  console.log(value);
 }
