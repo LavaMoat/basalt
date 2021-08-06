ea5ba84e22936f0b8820b40b9e9fbd679c79c013
muji <muji@tmpfs.org>
2021-08-06 08:50:16 +0800

    Fix policy builtin test specs.

--- tests/policy_builtin.rs
+++ tests/policy_builtin.rs
@@ -9,50 +9,26 @@ fn load_policy_test(dir: &str) -> Result<(String, String)> {
         std::fs::read_to_string(PathBuf::from(dir).join("output.json"))?;
     let file = PathBuf::from(dir).join("input.js");
     let builder = PolicyBuilder::new(file);
-    let policy = builder.load()?.analyze()?.finalize();
+    let policy = builder.load()?.flatten()?.group()?.analyze()?.finalize();
     let result = serde_json::to_string_pretty(&policy)?;
     Ok((expected.trim_end().to_string(), result))
 }
 
-#[test]
-fn policy_builtin_esm() -> Result<()> {
-    let (expected, result) = load_policy_test("tests/policy/builtin/esm")?;
-    //println!("{}", result);
-    assert_eq!(expected, result);
-    Ok(())
-}
+const MODULES: &[&str] = &[
+    "tests/policy/builtin/esm",
+    "tests/policy/builtin/cjs",
+    "tests/policy/builtin/named-import",
+    "tests/policy/builtin/named-require",
+    "tests/policy/builtin/named-deep",
+];
 
 #[test]
-fn policy_builtin_cjs() -> Result<()> {
-    let (expected, result) = load_policy_test("tests/policy/builtin/cjs")?;
-    println!("{}", result);
-    assert_eq!(expected, result);
-    Ok(())
-}
-
-#[test]
-fn policy_builtin_named_import() -> Result<()> {
-    let (expected, result) =
-        load_policy_test("tests/policy/builtin/named-import")?;
-    //println!("{}", result);
-    assert_eq!(expected, result);
-    Ok(())
-}
-
-#[test]
-fn policy_builtin_named_require() -> Result<()> {
-    let (expected, result) =
-        load_policy_test("tests/policy/builtin/named-require")?;
-    //println!("{}", result);
-    assert_eq!(expected, result);
-    Ok(())
-}
-
-#[test]
-fn policy_builtin_named_deep() -> Result<()> {
-    let (expected, result) =
-        load_policy_test("tests/policy/builtin/named-deep")?;
-    //println!("{}", result);
-    assert_eq!(expected, result);
+fn policy_builtin_resources() -> Result<()> {
+    for dir in MODULES {
+        println!("Run policy builtin spec {:#?}", dir);
+        let (expected, result) = load_policy_test(dir)?;
+        //println!("{}", result);
+        assert_eq!(expected, result);
+    }
     Ok(())
 }
